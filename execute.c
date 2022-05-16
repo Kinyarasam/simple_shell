85560d0c0dc95c319a315444cc687c1e6ccbbb5c
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-16 06:54:14 +0300

    Cleaned the repository

--- execute.c
+++ execute.c
@@ -133,15 +133,3 @@ void _print_int(int number)
 	for (; tmp >= 1; tmp /= 10)
 		count += _putchar(((i / tmp) % 10) + '0');
 }
-
-/**
- * _putchar - writes the character c to stdout.
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
