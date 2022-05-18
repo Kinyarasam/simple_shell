4ead7cac1102e363ed919c9b4df55c44401998db
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-18 20:26:56 +0300

    Merged to my personal shelly project

--- shell.c
+++ shell.c
@@ -1,42 +1,73 @@
 #include "main.h"
 
 /**
- * main - interactive command line interpreter
- * Return: Zero if successful.
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-
-int main(void)
+void free_data(data_shell *datash)
 {
-	char *string, **tokens;
-	size_t n = 0;
-	ssize_t gl, fd = 0;
-	int status = 0, tty = 1, is_builtin, error = 0;
+	unsigned int i;
 
-	isatty(STDIN_FILENO) == 0 ? tty = 0 : tty;
-	do {
-		string = NULL, tokens = NULL, n = 0, status = 0;
-		tty == 1 ? fd = write(STDOUT_FILENO, "($) ", 4) : tty;
-		if (fd == -1)
-			dprintf(STDERR_FILENO, "Can't write the stdout");
-		gl = getline(&string, &n, stdin);
-		if (gl == -1)
-		{
-			free(string);
-			if (feof(stdin))
-				return (error);
-			else
-				return (EXIT_FAILURE);
-		}
-		tokens = _strtok(tokens, string, " \t\n\"\'");
-		is_builtin = check_builtins(tokens, string, &error);
-		if (is_builtin)
-			status = 1;
-		if (status == 0)
-			check_execution(tokens, string, &error);
-		else
-			free(tokens);
-		free(string);
-	} while (1);
-	free(tokens);
-	return (0);
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
